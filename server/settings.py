c8d1595285dd82211cb2bebf9e760a94bb916d0b
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-21 11:08:17 +0100

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': 
     {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'films',
+        'NAME': '',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
